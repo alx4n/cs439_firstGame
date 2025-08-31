079ec6b41f3e77cfde84b5c4814b745253e7c411
alx4n <nbratek99@gmail.com>
2025-08-30 20:25:30 -0400

    Make screen and player classes

--- firstGame.py
+++ firstGame.py
@@ -1,7 +1,18 @@
 import pygame, simpleGE, random
 
+class Game(simpleGE.Scene):
+    def __init__(self):
+        super().__init__()
+        self.player = Player(self)
+        self.sprites = [self.player]
+
+class Player(simpleGE.Sprite):
+    def __init__(self, scene):
+        super().__init__(scene)
+
 def main():
-    print("Hello world")
+    game = Game()
+    game.start()
 
 if __name__ == "__main__":
     main()
